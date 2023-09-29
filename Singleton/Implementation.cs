4170642d45a694a658941d9159597e63752d703c
Andrey Cometores <anicometores@gmail.com>
2023-09-29 11:06:13 +0200

    Singleton with thread-safety

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,4 +1,29 @@
 ﻿namespace Singleton
 {
-  
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        // Lazy<T> for thread safety
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+        
+        /// <summary>
+        /// Instance
+        /// </summary>
+        public static Logger Instance => _lazyLogger.Value;
+
+        protected Logger()
+        {
+        }
+
+        /// <summary>
+        /// Singleton operation
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
 }
