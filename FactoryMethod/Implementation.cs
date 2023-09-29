885d158af730fd6ed2ce106ccecddf668cbe0e57
Andrey Cometores <anicometores@gmail.com>
2023-09-29 16:36:13 +0200

    FactoryMethod init

--- FactoryMethod/Implementation.cs
+++ FactoryMethod/Implementation.cs
@@ -1,4 +1,56 @@
 ﻿namespace FactoryMethod
 {
-   
+    /// <summary>
+    /// Product
+    /// </summary>
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+        public override string ToString() => GetType().Name;
+    }
+
+    /// <summary>
+    /// Concrete product
+    /// </summary>
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_countryIdentifier)
+                {
+                    case "EN":
+                        return 20;
+                    default:
+                        return 10;
+                }
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Concrete product
+    /// </summary>
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercentage
+        {
+            get => 15;
+        }
+    }
+    
+    
 }
