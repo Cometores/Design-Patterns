5130996ed150e34279df34b154ffcc3822e8c733
Andrey Komissarov <anicometores@gmail.com>
2024-01-12 14:23:16 +0100

    Virtual proxy init

--- Proxy/Implementation.cs
+++ Proxy/Implementation.cs
@@ -1,3 +1,70 @@
 ﻿namespace Proxy
 {
+    /// <summary>
+    /// Subject
+    /// </summary>
+    public interface IDocument
+    {
+        void DisplayDocument();
+    }
+
+    /// <summary>
+    /// RealSubject
+    /// </summary>
+    public class Document : IDocument
+    {
+        public string? Title { get; private set; }
+        public string? Content { get; private set; }
+        public int AuthorId { get; private set;  }
+        public DateTimeOffset LastAccessed { get; private set; }
+        private string _fileName;
+
+        public Document(string fileName)
+        {
+            _fileName = fileName;
+            LoadDocument(fileName);
+        }
+
+        private void LoadDocument(string fileName)
+        {
+            Console.WriteLine("Executing expensive action: loading a file from disk");
+            // fake loading...
+            Thread.Sleep(1000);
+
+            Title = "An expensive document";
+            Content = "Lots and lots of content";
+            AuthorId = 1;
+            LastAccessed = DateTimeOffset.UtcNow;
+        }
+
+        public void DisplayDocument()
+        {
+            Console.WriteLine($"Title: {Title}, Content: {Content}");
+        }
+    }
+
+    /// <summary>
+    /// Proxy
+    /// </summary>
+    public class DocumentProxy : IDocument
+    {
+        private string _fileName;
+        private Document? _document;
+
+        public DocumentProxy(string fileName)
+        {
+            _fileName = fileName;
+        }
+        
+        public void DisplayDocument()
+        {
+            // avoid creating the document until we need it
+            if (_document == null)
+            {
+                _document = new Document(_fileName);
+            }
+            
+            _document.DisplayDocument();
+        }
+    }
 }
