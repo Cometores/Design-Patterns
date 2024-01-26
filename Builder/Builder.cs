912c3654f38f90c5625d798093b96c582ce8585e
Andrey Komissarov <anicometores@gmail.com>
2024-01-26 18:38:27 +0100

    Builder pattern

---
+++ Builder/Builder.cs
@@ -0,0 +1,57 @@
+﻿namespace BuilderPattern;
+
+/// <summary>
+/// Builder
+/// </summary>
+public abstract class CarBuilder
+{
+    public Car Car { get; private set; }
+
+    public CarBuilder(string carType)
+    {
+        Car = new Car(carType);
+    }
+
+    public abstract void BuildEngine();
+    public abstract void BuildFrame();
+}
+
+/// <summary>
+/// ConcreteBuilder1 class
+/// </summary>
+public class MiniBuilder : CarBuilder
+{
+    public MiniBuilder() : base("Mini")
+    {
+    }
+
+    public override void BuildEngine()
+    {
+        Car.AddPart("'not a V8'");
+    }
+
+    public override void BuildFrame()
+    {
+        Car.AddPart("'3-door with stripes'");
+    }
+}
+
+/// <summary>
+/// ConcreteBuilder2 class
+/// </summary>
+public class BMWBuilder : CarBuilder
+{
+    public BMWBuilder() : base("BMW")
+    {
+    }
+
+    public override void BuildEngine()
+    {
+        Car.AddPart("'a fancy V8 engine'");
+    }
+
+    public override void BuildFrame()
+    {
+        Car.AddPart("'5-door with metallic finish'");
+    }
+}
---
+++ Builder/Director.cs
@@ -0,0 +1,26 @@
+﻿namespace BuilderPattern;
+
+/// <summary>
+/// Director
+/// </summary>
+public class Garage
+{
+    private CarBuilder? _builder;
+
+    public Garage()
+    {
+    }
+
+    public void Construct(CarBuilder builder)
+    {
+        _builder = builder;
+        
+        _builder.BuildEngine();
+        _builder.BuildFrame();
+    }
+
+    public void Show()
+    {
+        Console.WriteLine(_builder?.Car.ToString());
+    }
+}
