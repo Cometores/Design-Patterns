1d95f0610fa1a5e52674612cdd82042cf9292cbe
Andrey Komissarov <anicometores@gmail.com>
2024-03-04 17:39:17 +0100

    Template method init

--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Template Method";
+﻿using TemplateMethod;
+
+Console.Title = "Template Method";
+
+ExchangeMailParser exchangeMailParser = new();
+Console.WriteLine(exchangeMailParser.ParseMailBody("exchange"));
+Console.WriteLine();
+
+ApacheMailParser apacheMailParser = new();
+Console.WriteLine(apacheMailParser.ParseMailBody("apache"));
+Console.WriteLine();
+
+EudoraMailParser eudoraMailParser = new ();
+Console.WriteLine(eudoraMailParser.ParseMailBody("eudora"));
+
+Console.ReadKey();
