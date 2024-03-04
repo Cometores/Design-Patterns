a5e70e3483ef0c9308a26aa03984347898d1cf84
Andrey Komissarov <anicometores@gmail.com>
2024-03-04 18:19:43 +0100

    Strategy init

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.Title = "Strategy";
+﻿using Strategy;
+
+Console.Title = "Strategy";
+
+var order = new Order("Marvin Software", 5, "Visual Studio License");
+order.ExportService = new CSVExportService();
+order.Export();
+
+order.ExportService = new JsonExportService();
+order.Export();
+
+Console.ReadKey();
