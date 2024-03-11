cb76fe3d95bd54ad98f501e1606ca82df91dbe87
Andrey Komissarov <anicometores@gmail.com>
2024-03-11 15:46:17 +0100

    iterator init

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.Title = "Iterator";
+﻿using Iterator;
+
+Console.Title = "Iterator";
+
+// create the collection
+PeopleCollection people = new()
+{
+    new Person("Tom", "USA"),
+    new Person("Bob", "Belgium"),
+    new Person("Sam", "The Netherlands"),
+    new Person("Kate", "Germany")
+};
+
+// create the iterator
+var peopleIterator = people.CreateIterator();
+
+// use the iterator to run through the people int he collection
+// they should come out in alphabetical order
+for (Person person = peopleIterator.First(); !peopleIterator.IsDone; person = peopleIterator.Next())
+{
+    Console.WriteLine(person?.Name);
+}
+
+Console.ReadKey();
